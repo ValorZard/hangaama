b092c06eb9a97e6fe0562836b1e61286645380d1
Srayan Jana <valorzard.kiko@outlook.com>
2024-07-23 13:30:47 -0700

    add index buffer

--- src/lib.rs
+++ src/lib.rs
@@ -46,10 +46,21 @@ impl Vertex {
     }
 }
 
+// store all UNIQUE vertices
+// this saves a lot of memory compared to storing every single vertex
 const VERTICES: &[Vertex] = &[
-    Vertex {position:[0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0]},
-    Vertex {position:[-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0]},
-    Vertex {position:[0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0]},
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+// we can reuse vertices to create the triangles
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
 ];
 
 struct State<'a> {
@@ -61,6 +72,8 @@ struct State<'a> {
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
     // window must be declared after surface so it gets dropped after it
     // surface contains unsafe references to window's references
     window: &'a Window,
@@ -203,8 +216,16 @@ impl<'a> State<'a> {
                 usage: wgpu::BufferUsages::VERTEX,
             }
         );
-
         let num_vertices = VERTICES.len() as u32;
+
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+        let num_indices = INDICES.len() as u32;
 
         Self {
             window,
@@ -216,6 +237,8 @@ impl<'a> State<'a> {
             render_pipeline,
             vertex_buffer,
             num_vertices,
+            index_buffer,
+            num_indices,
         }
 
     }
@@ -287,7 +310,10 @@ impl<'a> State<'a> {
             render_pass.set_pipeline(&self.render_pipeline);
             // have to set vertex buffer in render method, else everything will crash
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1);
+            // we can have only one index buffer at a time
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            // we're using draw_indexed not draw(), since draw ignores index buffer.
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
