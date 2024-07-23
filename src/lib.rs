a00aa829ac62b91b5a18dba51b5dbf88416941ef
Srayan Jana <valorzard.kiko@outlook.com>
2024-07-23 13:14:51 -0700

    woops i had the wrong vertex

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@ impl Vertex {
 const VERTICES: &[Vertex] = &[
     Vertex {position:[0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0]},
     Vertex {position:[-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0]},
-    Vertex {position:[0.0, -0.5, 0.0], color: [0.0, 0.0, 1.0]},
+    Vertex {position:[0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0]},
 ];
 
 struct State<'a> {
