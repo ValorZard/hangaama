5252d93641cbec9ed6b18c9123ad8945b1e8fbc4
Srayan Jana <valorzard.kiko@outlook.com>
2024-09-10 13:57:46 -0700

    move input into its own file

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use glyphon::{
 use crate::sprite::*;
 mod camera;
 use camera::*;
+mod input;
+use input::InputStruct;
 use winit::{
     event::*,
     event_loop::EventLoop,
@@ -744,66 +746,6 @@ impl<'a> State<'a> {
         self.asset_map.clear()
     }
 
-}
-
-pub struct InputStruct {
-    is_up_pressed: bool,
-    is_down_pressed: bool,
-    is_left_pressed: bool,
-    is_right_pressed: bool,
-    pub is_space_pressed: bool,
-}
-
-impl InputStruct {
-    pub fn new() -> Self {
-        Self {
-            is_up_pressed: false,
-            is_down_pressed: false,
-            is_left_pressed: false,
-            is_right_pressed: false,
-            is_space_pressed: false,
-        }
-    }
-
-    pub fn process_events(&mut self, event: &WindowEvent) -> bool {
-        match event {
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        state,
-                        physical_key: PhysicalKey::Code(keycode),
-                        ..
-                    },
-                ..
-            } => {
-                let is_pressed = *state == ElementState::Pressed;
-                match keycode {
-                    KeyCode::KeyW | KeyCode::ArrowUp => {
-                        self.is_up_pressed = is_pressed;
-                        true
-                    }
-                    KeyCode::KeyA | KeyCode::ArrowLeft => {
-                        self.is_left_pressed = is_pressed;
-                        true
-                    }
-                    KeyCode::KeyS | KeyCode::ArrowDown => {
-                        self.is_down_pressed = is_pressed;
-                        true
-                    }
-                    KeyCode::KeyD | KeyCode::ArrowRight => {
-                        self.is_right_pressed = is_pressed;
-                        true
-                    }
-                    KeyCode::Space => {
-                        self.is_space_pressed = is_pressed;
-                        true
-                    }
-                    _ => false,
-                }
-            }
-            _ => false,
-        }
-    }
 }
 
 struct Player {
